e0dd239f79eed0599e87d592a23d8d6e2b683615
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2021-12-31 20:00:20 -0500

    Added style components

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,16 +1,16 @@
-import Head from "next/head";
+import Head from 'next/head';
 
-/* This example requires Tailwind CSS v2.0+ */
-function Layout({children}) {
-    return <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">{children}</div>
-  }
-  
+type Children = React.ReactNode;
 
-export const Page: React.FC<PageProps> = ({ children }) => (
-    <>
-        <Head>
-            <title>Drone Store Concept</title>
-        </Head>
-        <Layout>{children}</Layout>
-    </>
+const Layout: React.FC<Children> = ({ children }) => {
+  return <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">{children}</div>;
+};
+
+export const Page: React.FC<Children> = ({ children }) => (
+  <>
+    <Head>
+      <title>Drone Store Concept</title>
+    </Head>
+    <Layout>{children}</Layout>
+  </>
 );
---
+++ src/pages/_document.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const { renderPage } = ctx;
+
+    try {
+      ctx.renderPage = () =>
+        renderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en-ZA">
+        <Head>
+          <meta charSet="utf-8" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Reenie+Beanie&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
