29bf3edb89bc856311e1ca78a0a926c5fa550e44
Matthew Thomas <matthewthomas@Matthews-MacBook-Pro.local>
2022-01-07 14:51:16 -0500

    Add to Cart Loader

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactLoader from 'react-loader-spinner';
 import { useCart } from '../lib/cartState';
 import { ProductWithQuantity } from '../types/ProductWithQuantity';
 
@@ -7,10 +8,15 @@ type AddToCartProps = {
 };
 
 const AddToCartButton: React.FC<AddToCartProps> = ({ product }) => {
+  const [loading, setLoading] = useState(false);
   const { addToCart } = useCart();
 
   const handleAddToCart = () => {
-    addToCart(product); // TODO: Needs to add X to whats already there
+    setLoading(true);
+    setTimeout(() => {
+      addToCart(product);
+      setLoading(false);
+    }, 1000);
   };
 
   return (
@@ -19,7 +25,16 @@ const AddToCartButton: React.FC<AddToCartProps> = ({ product }) => {
       className="w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
       onClick={handleAddToCart}
     >
-      Add to bag
+      {loading ? (
+        <ReactLoader
+          type="TailSpin"
+          color="white"
+          height="1.5rem"
+          timeout={0}
+        />
+      ) : (
+        'Add to cart'
+      )}
     </button>
   );
 };
